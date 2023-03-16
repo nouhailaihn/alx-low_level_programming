24eda080e31fbc8cb63996d510daebb80c9ab9ef
nouhailaihn <nouhaila.ihnich@gmail.com>
2023-03-16 16:35:31 -0700

    idk

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,5 +8,5 @@ printf("Size of a long int: %lu byte(s)\n", sizeof(long));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
 printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
-return(0);
+return (0);
 }
