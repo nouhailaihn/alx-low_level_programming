b6525cbc95caee7d2cd13255cdae977da9e7ec56
nouhailaihn <nouhaila.ihnich@gmail.com>
2023-03-16 16:42:53 -0700

    another set of updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-echo 'Programming is like building a multilingual puzzle,' | tr -d '\n'
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
